4d38c8ad757c638661d9d1ed2ed9aaacba8a6ab6
ibrahim mahmoud elghandour <ibrahimelghandour51@gmail.com>
2026-02-01 20:41:37 +0200

    FINISHING THE COMPONENTS STRUCTURE

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from "./components/navbar/navbar";
+import { Home } from "./components/home/home";
+import { AboutUs } from "./components/about-us/about-us";
+import { Blog } from "./components/blog/blog";
+import { NotFound } from "./components/not-found/not-found";
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar, Home, AboutUs, Blog, NotFound,Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
