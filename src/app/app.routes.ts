8abfa7b2fa26f045201aaecd2176ef5680fe0f92
ibrahim mahmoud elghandour <ibrahimelghandour51@gmail.com>
2026-02-04 13:21:30 +0200

    Refactor project structure and update styles. Added new footer and hero components, integrated routing, and updated Angular dependencies. Replaced logo and adjusted navbar styles for better UI. Updated Arabic localization throughout the application.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Blog } from './components/blog/blog';
+import { AboutUs } from './components/about-us/about-us';
+import { NotFound } from './components/not-found/not-found';
+import { Home } from './components/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: Home },
+  { path: 'blog', component: Blog },
+  { path: 'about', component: AboutUs },
+  { path: '**', component: NotFound },
+];
